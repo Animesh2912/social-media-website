28b1431b2154d968e16287148539cc9ab3c73fac
animesh <animeshpandey775@gmail.com>
2024-06-25 17:37:50 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
+
+function App() {
+  const [SelectedTab, setSelectedTab] = useState("Home");
+
+  return (
+    <div className="app-container">
+      <Sidebar
+        SelectedTab={SelectedTab}
+        setSelectedTab={setSelectedTab}
+      ></Sidebar>
+      <div className="content">
+        <Header></Header>
+        {SelectedTab === "Home" ? (
+          <PostList></PostList>
+        ) : (
+          <CreatePost></CreatePost>
+        )}
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
